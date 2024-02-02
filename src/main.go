a4ab4391ce9ff9ef7959674c9802706d0fc4e38c
PlayJeri <numri.jeri@gmail.com>
2024-02-02 16:04:28 +0200

    Refactor main function to use render package

--- src/main.go
+++ src/main.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/mattn/go-runewidth"
-	"github.com/nsf/termbox-go"
+	"github.com/PlayJeri/text-editor/src/render"
 )
 
-func print_message(col, row int, fg, bg termbox.Attribute, message string) {
-	for _, ch := range message {
-		termbox.SetCell(col, row, ch, fg, bg)
-		col += runewidth.RuneWidth(ch)
-	}
-}
-
-func run_editor() {
-	err := termbox.Init()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	for {
-		print_message(25, 11, termbox.ColorGreen, termbox.ColorBlack, "ISO - TEXT EDITOR")
-		termbox.Flush()
-		event := termbox.PollEvent()
-		if event.Type == termbox.EventKey && event.Key == termbox.KeyEsc {
-			termbox.Close()
-			break
-		}
-	}
-}
-
 func main() {
-	run_editor()
+	render.Run_editor()
 }
