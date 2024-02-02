f84e2d11fe0e8c2c0631612bd481b02f5dd8ab31
PlayJeri <numri.jeri@gmail.com>
2024-02-02 16:04:33 +0200

    Add display_text_buffer function and Run_editor function

--- src/render/core.go
+++ src/render/core.go
@@ -1,18 +1,67 @@
 package render
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/mattn/go-runewidth"
 	"github.com/nsf/termbox-go"
 )
 
-func Run() {
+var ROWS, COLS int
+var offsetX, offsetY int
+
+var text_buffer = [][]rune{
+	{'H', 'e', 'l', 'l', 'o', ','},
+	{'W', 'o', 'r', 'l', 'd', '!'},
+}
+
+func display_text_buffer() {
+	var row, col int
+	for row = 0; row < ROWS; row++ {
+		text_bufferRow := row + offsetY
+		for col = 0; col < COLS; col++ {
+			text_bufferCol := col + offsetX
+			if text_bufferRow >= 0 && text_bufferRow < len(text_buffer) && text_bufferCol < len(text_buffer[text_bufferRow]) {
+				if text_buffer[text_bufferRow][text_bufferCol] != '\t' {
+					termbox.SetChar(col, row, text_buffer[text_bufferRow][text_bufferCol])
+				} else {
+					termbox.SetCell(col, row, rune(' '), termbox.ColorDefault, termbox.ColorGreen)
+				}
+			} else if row+offsetY > len(text_buffer)-1 {
+				termbox.SetCell(0, row, rune('*'), termbox.ColorBlue, termbox.ColorDefault)
+			}
+		}
+		termbox.SetChar(col, row, rune('\n'))
+	}
+}
+
+func Run_editor() {
 	err := termbox.Init()
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	defer termbox.Close()
-
-	// Main event loop
 	for {
+		ROWS, COLS = termbox.Size()
+		ROWS--
+		if COLS < 78 {
+			COLS = 78
+		}
+		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+		display_text_buffer()
+		termbox.Flush()
+		event := termbox.PollEvent()
+		if event.Type == termbox.EventKey && event.Key == termbox.KeyEsc {
+			termbox.Close()
+			break
+		}
+	}
+}
 
+func Print_message(col, row int, fg, bg termbox.Attribute, message string) {
+	for _, ch := range message {
+		termbox.SetCell(col, row, ch, fg, bg)
+		col += runewidth.RuneWidth(ch)
 	}
 }
